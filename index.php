9b232e25e1aa422bd14e5e1e644298ea220a826f
Iqbolshoh <iilhomjonov777@gmail.com>
2024-12-31 10:41:15 +0500

    commit

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ $data = [
     'year' => 2022,
     'price' => 25000.99
 ];
+
 $insert_result = $query->insert('cars', $data); // Insert data into the cars table
 if ($insert_result) {
     echo "Car added successfully with ID: " . $insert_result . "<br>";
@@ -20,37 +21,14 @@ if ($insert_result) {
     echo "Error occurred while adding the car!<br>";
 }
 
-// // SELECT example: Retrieving all cars from the database
-// $cars = $query->select('cars'); // Select all cars
-// if (is_array($cars) && count($cars) > 0) {
-//     echo "<h3>Cars in the database:</h3><ul>";
-//     foreach ($cars as $car) {
-//         echo "<li>ID: " . $car['id'] . " | Make: " . $car['make'] . " | Model: " . $car['model'] . " | Year: " . $car['year'] . " | Price: $" . $car['price'] . "</li>";
-//     }
-//     echo "</ul>";
-// } else {
-//     echo "No cars found in the database.<br>";
-// }
-
-// // UPDATE example: Updating a car's price
-// $update_data = [
-//     'price' => 23000.99
-// ];
-// $update_condition = "id = ?";
-// $update_params = [1]; // ID of the car to update
-// $update_result = $query->update('cars', $update_data, $update_condition, $update_params, 'i');
-// if ($update_result > 0) {
-//     echo "Car updated successfully!<br>";
-// } else {
-//     echo "Error occurred while updating the car!<br>";
-// }
-
-// // DELETE example: Deleting a car from the database
-// $delete_condition = "id = ?";
-// $delete_params = [1]; // ID of the car to delete
-// $delete_result = $query->delete('cars', $delete_condition, $delete_params, 'i');
-// if ($delete_result > 0) {
-//     echo "Car deleted successfully!<br>";
-// } else {
-//     echo "Error occurred while deleting the car!<br>";
-// }
+// SELECT example: Retrieving all cars from the database
+$cars = $query->select('cars'); // Select all cars
+if (is_array($cars) && count($cars) > 0) {
+    echo "<h3>Cars in the database:</h3><ul>";
+    foreach ($cars as $car) {
+        echo "<li>ID: " . $car['id'] . " | Make: " . $car['make'] . " | Model: " . $car['model'] . " | Year: " . $car['year'] . " | Price: $" . $car['price'] . "</li>";
+    }
+    echo "</ul>";
+} else {
+    echo "No cars found in the database.<br>";
+}
