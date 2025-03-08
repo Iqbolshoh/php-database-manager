6ebf72baa693061066e68fe009aa05de1bbab6f3
Iqbolshoh Ilhomjonov <148738947+Iqbolshoh@users.noreply.github.com>
2025-03-08 22:21:18 +0500

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 define("DB_SERVER", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
-define("DB_NAME", "example_database");
+define("DB_NAME", "php_database_menager");
 
 // Dynamically determine the base site URL
 define("SITE_PATH", ($_SERVER['REQUEST_SCHEME'] ?? 'http') . "://" . $_SERVER['HTTP_HOST']);
