8caf873ec8b88ee7da2b12c0d383cd99db360811
iqbolshoh <iilhomjonov777@gmail.com>
2025-03-09 09:55:53 +0500

    commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once './config.php'; // Include the Database class
+require_once './database.php'; // Include the Database class
 $db = new Database(); // Create a new Database instance
 
 // ➕ Insert a new user
